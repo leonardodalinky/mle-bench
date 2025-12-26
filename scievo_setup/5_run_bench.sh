7eb0ee768358843a4cdc0316102c12c183223dfc
leonardodalinky <linkyy2000313@gmail.com>
2025-12-26 11:46:34 -0500

    chore: update

--- scievo_setup/5_run_bench.sh
+++ scievo_setup/5_run_bench.sh
@@ -5,8 +5,8 @@
 
 # alternative command with GPU config
 # python run_agent.py --agent-id scievo/gemini-low-medium --competition-set experiments/splits/low.txt --container-config environment/config/container_configs/gpu.json
-python run_agent.py --agent-id scievo/gemini-medium-high --competition-set experiments/splits/low.txt --container-config environment/config/container_configs/gpu.json
-# python run_agent.py --agent-id scievo/gemini-medium-high --competition-set experiments/splits/low_rest_gemini.txt --container-config environment/config/container_configs/gpu.json
+# python run_agent.py --agent-id scievo/gemini-medium-high --competition-set experiments/splits/low.txt --container-config environment/config/container_configs/gpu.json
+python run_agent.py --agent-id scievo/gemini-medium-high --competition-set experiments/splits/low_rest_gemini.txt --container-config environment/config/container_configs/gpu.json
 
 # python run_agent.py --agent-id scievo/gpt-low-medium --competition-set experiments/splits/low.txt --container-config environment/config/container_configs/gpu.json
 # python run_agent.py --agent-id scievo/gpt-medium-high --competition-set experiments/splits/low.txt --container-config environment/config/container_configs/gpu.json
--- scievo_setup/6_grading.sh
+++ scievo_setup/6_grading.sh
@@ -3,7 +3,8 @@
 cd "$(dirname "$0")/.."
 
 
-EXP_ID=2025-12-23T03-27-25-GMT_run-group_scievo
+# EXP_ID=2025-12-24T11-26-49-GMT_run-group_scievo_gpt
+EXP_ID=tmp_gpt
 
 python experiments/make_submission.py \
     --metadata runs/${EXP_ID}/metadata.json \
